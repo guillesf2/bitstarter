46f8d4d5092bd1db34ef526c4e3376ea3c6783bc
Guillermo Serrano <jguillesf2@gmail.com>
2013-07-09 02:40:23 +0000

    bien

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+ //response.send('Hello World 3!');
+    var text = fs.readFile(index.html);
+    response.send('text');
 });
 
 var port = process.env.PORT || 5000;

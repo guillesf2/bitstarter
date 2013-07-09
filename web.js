d3b20ca6c287d2c96f1840316d65b41f408cda22
Guillermo Serrano <jguillesf2@gmail.com>
2013-07-09 02:49:59 +0000

    hola

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
  //response.send('Hello World 3!');
-    var text = fs.readFileSync(index.html,'utf8');
+    var text = fs.readFileSync('./index.html','utf8');
     response.send('text');
 });
 
